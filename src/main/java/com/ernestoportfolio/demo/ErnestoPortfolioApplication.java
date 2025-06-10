ce6a33ec1679b8e28cd76cf9ea539d722e416fc9
ernestocarocca <113439130+ernestocarocca@users.noreply.github.com>
2025-06-10 09:05:59 +0200

    testing upstream

--- src/main/java/com/ernestoportfolio/demo/ErnestoPortfolioApplication.java
+++ src/main/java/com/ernestoportfolio/demo/ErnestoPortfolioApplication.java
@@ -2,7 +2,7 @@ package com.ernestoportfolio.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//new commit
 @SpringBootApplication
 public class ErnestoPortfolioApplication {
 	public static void main(String[] args) {
